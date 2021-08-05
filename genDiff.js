04187252e749337716607ea0a8fce1bc6f3b5dc5
PerCreate <kostya.pershin007@gmail.com>
2021-08-05 12:20:15 +0400

    modified

--- genDiff.js
+++ genDiff.js
@@ -1,27 +1,27 @@
-import fs from "fs";
+import fs from 'fs';
 
-export default(filepath1, filepath2) => {
-    const file1 = JSON.parse(fs.readFileSync(filepath1, { encoding: "utf8", flag: "r" }));
-    const file2 = JSON.parse(fs.readFileSync(filepath2, { encoding: "utf8", flag: "r" }));
-    const keysFile1 = Object.keys(file1);
-    const keysFile2 = Object.keys(file2);
-    const commonKeys = keysFile1.concat(keysFile2.filter((key) => (
-        keysFile1.indexOf(key) === -1
-    ))).sort();
-    let sortedObj = {};
-    for (const key of commonKeys) {
-        if (Object.prototype.hasOwnProperty.call(file1, key)) {
-            if (Object.prototype.hasOwnProperty.call(file2, key)) {
-                if (file1[key] === file2[key]) sortedObj[`  ${key}`] = file1[key];
-                else {
-                    sortedObj[`- ${key}`] = file1[key];
-                    sortedObj[`+ ${key}`] = file2[key];
-                }
-            }
-            else sortedObj[`- ${key}`] = file1[key];
+export default (filepath1, filepath2) => {
+  const file1 = JSON.parse(fs.readFileSync(filepath1, { encoding: 'utf8', flag: 'r' }));
+  const file2 = JSON.parse(fs.readFileSync(filepath2, { encoding: 'utf8', flag: 'r' }));
+  const keysFile1 = Object.keys(file1);
+  const keysFile2 = Object.keys(file2);
+  const commonKeys = keysFile1.concat(keysFile2.filter((key) => (
+    keysFile1.indexOf(key) === -1
+  ))).sort();
+  const sortedObj = {};
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key of commonKeys) {
+    if (Object.prototype.hasOwnProperty.call(file1, key)) {
+      if (Object.prototype.hasOwnProperty.call(file2, key)) {
+        if (file1[key] === file2[key]) sortedObj[`  ${key}`] = file1[key];
+        else {
+          sortedObj[`- ${key}`] = file1[key];
+          sortedObj[`+ ${key}`] = file2[key];
         }
-        else sortedObj[`+ ${key}`] = file2[key];
-    }
-    console.log(sortedObj);
-    return sortedObj;
+      } else sortedObj[`- ${key}`] = file1[key];
+    } else sortedObj[`+ ${key}`] = file2[key];
+  }
+  // eslint-disable-next-line no-console
+  console.log(sortedObj);
+  return sortedObj;
 };
--- gendiff.mjs
+++ gendiff.mjs
@@ -1,13 +1,13 @@
-import { Command } from "commander/esm.mjs";
-import genDiff from "./genDiff";
+import { Command } from 'commander';
+import genDiff from './genDiff.js';
+
 const program = new Command();
 program
-    .version("0.0.4", "-V, --version', 'output the version number")
-    .description("Compares two configuration files and shows a difference.")
-    .option("-f, --format [type]', 'output format")
-    .arguments("<filepath1> <filepath2>")
-    .action((filepath1, filepath2) => genDiff(filepath1, filepath2))
-    .parse();
-
+  .version('0.0.4', "-V, --version', 'output the version number")
+  .description('Compares two configuration files and shows a difference.')
+  .option("-f, --format [type]', 'output format")
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => genDiff(filepath1, filepath2))
+  .parse();
 
 export default program;
